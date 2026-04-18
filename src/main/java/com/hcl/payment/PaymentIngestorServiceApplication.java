a632278ccb0c17208909ed67ecb0760328b7e00b
Chalapathi Rao Davala <chalapathi.davala@gmail.com>
2026-04-18 15:44:34 +0530

    first

---
+++ src/main/java/com/hcl/payment/PaymentIngestorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIngestorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentIngestorServiceApplication.class, args);
+	}
+
+}
